18c10fe6e8113868287ddf35b68eb589f8f68600
vagamesint <147235389+vagamesint@users.noreply.github.com>
2025-02-20 17:20:17 +0530

    Added IOS Alert functionality

---
+++ Runtime/IosAlert.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+namespace vagamesint.native_app_utility
+{
+    public class IosAlert
+    {
+#if UNITY_IOS
+        [DllImport("__Internal")]
+        private static extern void ShowAlert(string title, string message);
+        [DllImport("__Internal")]
+        private static extern void ShowToast(string message, double timer);
+    
+
+#endif
+        public static void ShowAlertBox(string title, string message)
+        {
+            ShowAlert(title, message);
+        }
+
+        public static void ShowToast()
+        {
+            ShowToast("Some message...", 3.0);
+        }
+    }
+}
